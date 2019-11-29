81fd3da3466127e76a0344b4c32048863f074059
Ammon Smith <ammon.i.smith@gmail.com>
2019-11-29 02:30:50 -0500

    Add request tests.

--- src/test.rs
+++ src/test.rs
@@ -36,3 +36,213 @@ fn test_redirect() {
     check!("/SCP-1000", Some("/scp-1000"));
     check!("/scp-1000", None);
 }
+
+#[test]
+fn test_request() {
+    macro_rules! check {
+        ($path:expr, $expected:expr) => {{
+            let page_req = Request::parse($path);
+            assert_eq!(
+                page_req, $expected,
+                "Parsed Request doesn't match expected"
+            );
+        }};
+    }
+
+    check!(
+        "scp-1000",
+        Request {
+            slug: "scp-1000",
+            categories: vec![],
+            arguments: hashmap! {},
+        }
+    );
+    check!(
+        "scp-1000/edit",
+        Request {
+            slug: "scp-1000",
+            categories: vec![],
+            arguments: hashmap! { "edit" => ArgumentValue::Null },
+        }
+    );
+    check!(
+        "scp-1000/edit/1",
+        Request {
+            slug: "scp-1000",
+            categories: vec![],
+            arguments: hashmap! { "edit" => ArgumentValue::from(1) },
+        }
+    );
+    check!(
+        "scp-1000/edit/true",
+        Request {
+            slug: "scp-1000",
+            categories: vec![],
+            arguments: hashmap! { "edit" => ArgumentValue::from(true) },
+        }
+    );
+    check!(
+        "component:image-block",
+        Request {
+            slug: "image-block",
+            categories: vec!["component"],
+            arguments: hashmap! {},
+        }
+    );
+    check!(
+        "deleted:component:image-block",
+        Request {
+            slug: "image-block",
+            categories: vec!["deleted", "component"],
+            arguments: hashmap! {},
+        }
+    );
+    check!(
+        "fragment:scp-4447-1",
+        Request {
+            slug: "scp-4447-1",
+            categories: vec!["fragment"],
+            arguments: hashmap! {},
+        }
+    );
+    check!(
+        "fragment:scp-4447-1/discuss",
+        Request {
+            slug: "scp-4447-1",
+            categories: vec!["fragment"],
+            arguments: hashmap! { "discuss" => ArgumentValue::Null },
+        }
+    );
+    check!(
+        "fragment:scp-4447-1/discuss/true",
+        Request {
+            slug: "scp-4447-1",
+            categories: vec!["fragment"],
+            arguments: hashmap! { "discuss" => ArgumentValue::from(true) },
+        }
+    );
+    check!(
+        "scp-series-5",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! {},
+        }
+    );
+    check!(
+        "scp-series-5/norender",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::Null },
+        }
+    );
+    check!(
+        "scp-series-5/norender/1",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(1) },
+        }
+    );
+    check!(
+        "scp-series-5/norender/true",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(true) },
+        }
+    );
+    check!(
+        "scp-series-5/norender/true/noredirect",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(true), "noredirect" => ArgumentValue::Null },
+        }
+    );
+    check!(
+        "scp-series-5/norender/1/noredirect",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(1), "noredirect" => ArgumentValue::Null },
+        }
+    );
+    check!(
+        "scp-series-5/norender/true/noredirect",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(true), "noredirect" => ArgumentValue::Null },
+        }
+    );
+    check!(
+        "scp-series-5/norender/true/noredirect/true",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(true), "noredirect" => ArgumentValue::from(true) },
+        }
+    );
+    check!(
+        "scp-series-5/norender/1/noredirect/1",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(1), "noredirect" => ArgumentValue::from(1) },
+        }
+    );
+    check!(
+        "scp-series-5/norender/true/noredirect/true",
+        Request {
+            slug: "scp-series-5",
+            categories: vec![],
+            arguments: hashmap! { "norender" => ArgumentValue::from(true), "noredirect" => ArgumentValue::from(true) },
+        }
+    );
+    check!(
+        "aaa:page/true/false/true/false/edit",
+        Request {
+            slug: "page",
+            categories: vec!["aaa"],
+            arguments: hashmap! { "edit" => ArgumentValue::Null },
+        }
+    );
+    check!(
+        "aaa:page/true/false/true/false/edit/1",
+        Request {
+            slug: "page",
+            categories: vec!["aaa"],
+            arguments: hashmap! { "edit" => ArgumentValue::from(1) },
+        }
+    );
+    check!(
+        "aaa:page/true/false/true/false/edit/true",
+        Request {
+            slug: "page",
+            categories: vec!["aaa"],
+            arguments: hashmap! { "edit" => ArgumentValue::from(true) },
+        }
+    );
+    check!(
+        "aaa:bbb:page/noredirect/false/norender/0/true/false",
+        Request {
+            slug: "page",
+            categories: vec!["aaa", "bbb"],
+            arguments: hashmap! { "noredirect" => ArgumentValue::from(false), "norender" => ArgumentValue::from(0) },
+        }
+    );
+    check!(
+        "aaa:bbb:page/tags/tale/title/A Story/edit",
+        Request {
+            slug: "page",
+            categories: vec!["aaa", "bbb"],
+            arguments: hashmap! {
+                "tags" => ArgumentValue::from("tale"),
+                "title" => ArgumentValue::from("A Story"),
+                "edit" => ArgumentValue::Null,
+            },
+        }
+    );
+}
