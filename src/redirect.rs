1f5f5cbc993f0efe12863013543965f32c19ae0b
Ammon Smith <ammon.i.smith@gmail.com>
2020-01-25 18:39:50 -0500

    Strip _default: category for redirect.

--- src/redirect.rs
+++ src/redirect.rs
@@ -17,6 +17,9 @@ use wikidot_normalize::{is_normal, normalize_decode};
 ///
 /// This is to allow consumers in a web router to redirect requests which are
 /// not in Wikidot normal form.
+///
+/// This also redirects pages in the default category: `_default:page` will be
+/// redirected to simply `page`.
 pub fn redirect<S: Into<String>>(path: S) -> Option<String> {
     let mut path = path.into();
 
@@ -25,6 +28,11 @@ pub fn redirect<S: Into<String>>(path: S) -> Option<String> {
         None
     } else {
         normalize_decode(&mut path);
+
+        if path.starts_with("_default") {
+            path.replace_range(..9, "");
+        }
+
         Some(path)
     }
 }
